ae9fa8a9a13d3b1722dbfee20fbeb1392a4e247f
yongjian20 <55908596+yongjian20@users.noreply.github.com>
2021-12-02 11:18:10 +0800

    Update deploy.sh

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 set -x
-docker run -d -p 8000:80 --name my-apache-php-app -v $WORKSPACE/src:/var/www/html php:7.2-apache
+docker run -d -p 80:80 --name my-apache-php-app -v $WORKSPACE/src:/var/www/html php:7.2-apache
 sleep 1
 set +x
 
