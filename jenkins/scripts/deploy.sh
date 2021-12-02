e2f8b08bbc779c5e73e82bf5891fce51870f124c
yongjian20 <55908596+yongjian20@users.noreply.github.com>
2021-12-02 11:09:31 +0800

    Update deploy.sh

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env sh
 
 set -x
-docker run -d -p 8000:80 --name my-apache-php-app -v c:\\...\\jenkins-php-selenium-test\\src:/var/www/html php:7.2-apache
+docker run -d -p 8000:80 --name my-apache-php-app -v $WORKSPACE/src:/var/www/html php:7.2-apache
 sleep 1
 set +x
 
 echo 'Now...'
 echo 'Visit http://192.168.58.132:8080/:8000 to see your PHP application in action.'
-
